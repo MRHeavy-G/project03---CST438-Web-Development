54373cea34f3ff6d5d882d05e15920650981039d
Brendon Magana <56487444+MRHeavy-G@users.noreply.github.com>
2022-04-07 15:41:11 -0700

    Began the structure of the webbased app with springboot
    Added a basic html landing page. Nothing has been added based on the DB and other functions

---
+++ WebBasedApp/src/main/java/com/example/project03webbaseapp/Project03WebBaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.project03webbaseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project03WebBaseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project03WebBaseAppApplication.class, args);
+    }
+
+}
---
+++ WebBasedApp/src/test/java/com/example/project03webbaseapp/Project03WebBaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project03webbaseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project03WebBaseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
