15ede0647aa714b766fad998797bb50a66a46914
Brendon Magana <56487444+MRHeavy-G@users.noreply.github.com>
2022-04-26 16:41:31 -0700

    Worked on images but i cant get the nav bar to line up.
    Will continue working on layout and session next

--- WebBasedApp/src/main/java/com/example/project03webbaseapp/LandingPageController.java
+++ WebBasedApp/src/main/java/com/example/project03webbaseapp/LandingPageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LandingPageController {
 
-    @RequestMapping(value="/login", method= RequestMethod.GET)
+    @RequestMapping(value="/landing_page", method= RequestMethod.GET)
     public String getLogin() {
         return "landing_page";
     }
